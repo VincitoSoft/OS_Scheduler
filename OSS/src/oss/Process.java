dee8ed0ad30263f04324f1cb605f7c3f58e23f94
Thilina-Piyadasun <thilinapiyadasun@gmail.com>
2014-09-18 23:28:07 +0530

    create again process class

--- OSS/src/oss/Process.java
+++ OSS/src/oss/Process.java
@@ -1,6 +1,10 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package oss;
-
-import java.util.Observable;
+import java.util.*;
 /**
  *
  * @author Thilina Piyadasun
@@ -8,36 +12,34 @@ import java.util.Observable;
 public class Process extends Observable{
     
     private int processId;
-    private long serviceTime;
-    private long startTime;
+    private long processAvailableTime;
+    private long startTime=0;
     private long currntTime=0;
-    private static final long timeQuntm=5000;
-    
-    public Process(int processId,long SrvTime){ 
-        this.processId=processId;
-        this.serviceTime=SrvTime;
+    private static final long timeQunta=5000;
+
+    public Process(int processId,long serviceTime) {
+        this.processId = processId;
+        this.processAvailableTime=serviceTime;
     }
+    
     public void run(){
-            startTime = System.currentTimeMillis();
-            while(true){
-              
-                currntTime=System.currentTimeMillis();
-                if(serviceTime<=currntTime-startTime)
-                {
-                    notifyObservers();                  //end of the process
-                    break;
-                }
-                else if(timeQuntm<=currntTime-startTime){
-                    
-                    notifyObservers();
-                    serviceTime=serviceTime-timeQuntm;
-                    break;                             // process go to suspend state
-                }
-  
-            }
+        
+        currntTime=System.currentTimeMillis();
+        
+        while(true){
+            currntTime=System.currentTimeMillis();
             
+            if(processAvailableTime<=currntTime-startTime){
+                notifyObservers();                      //end of the process
+                break;
+            }
+            else if(timeQunta<=currntTime-startTime){
+                notifyObservers();
+                processAvailableTime=processAvailableTime-timeQunta;
+                break;                                 //process go to suspended state
+            }
         }
-
-
-
+    }
+            
 }
+
